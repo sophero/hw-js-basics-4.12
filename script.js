85edb7f2e251b2c025cf0288b923f135caa00e29
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-12 16:21:39 -0400

    Completes question 2.

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ var str1 = "Hello ";
 var str2 = "Ognian.";
 console.log(str1 + str2);
 
+
+// Question Two //
+
+var guitaristsArray = ["John Petrucci", "Tosin Abasi", "Guthrie Govan"];
+var drummersArray = ["Mike Portnoy", "Chris Pennie", "Gavin Harrison"];
+var multiArray = [guitaristsArray, drummersArray];
+
+console.log(multiArray[0][2]);
+console.log(multiArray[1][0]);
+
